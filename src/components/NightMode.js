1a887fb4adb9c872234b8d96872fb7da9cc32144
Gabe Kugelmass <gkord@me.com>
2019-12-08 11:53:47 -0500

    initial commit

---
+++ src/components/NightMode.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NightMode = () => (
+    <div>
+        NightMode!!!
+    </div>
+)
+
+export default NightMode;
---
+++ src/components/containers/Playlist.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PlaylistHeader from "../PlaylistHeader";
+import PlaylistItems from "../containers/PlaylistItems";
+import NightMode from "../NightMode";
+
+const Playlist = props => (
+    <>
+      <NightMode />
+      <PlaylistHeader />
+      <PlaylistItems />
+    </>
+  );
+
+export default Playlist;
